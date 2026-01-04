6124085b70d28f2651f87000e73fdf7db6e97723
t8y2 <1156263951@qq.com>
2026-01-04 14:33:25 +0800

    refactor(PluginMarketplace): Remove inline SVG spinner from refresh button
    - Remove inline SVG spinner icon from refresh button markup
    - Simplify button structure by removing unnecessary animation logic
    - Reduce component complexity and improve maintainability
    - Aligns with component simplification efforts

--- src/renderer/src/components/PluginMarketplace.vue
+++ src/renderer/src/components/PluginMarketplace.vue
@@ -334,23 +334,7 @@ onUnmounted(() => {
           </button>
         </div>
 
-        <Button :disabled="loading" @click="loadPlugins">
-          <svg
-            class="w-4 h-4 mr-2"
-            :class="{ 'animate-spin': loading }"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-            />
-          </svg>
-          刷新
-        </Button>
+        <Button :disabled="loading" @click="loadPlugins"> 刷新 </Button>
       </div>
     </div>
 
